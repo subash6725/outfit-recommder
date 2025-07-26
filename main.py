a5c5ce8bdc6be3dc82c5adfacc2ba0d0d24a4c9f
subash6725 <subashpalanisamy113@gmail.com>
2025-07-26 14:26:55 +0000

    Initial recommender version

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
     recommender = OutfitRecommender("data/outfits.csv")
     print("Recommendations for color='Red', occasion='Party':")
     print(recommender.recommend(color='Red', occasion='Party'))
-    
+
     if __name__ == "__main__":
       main()
     
